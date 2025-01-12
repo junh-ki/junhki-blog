8e239103cce8c3c9e8c5989f35eca15b16ac7be0
junh-ki <kijoonh91@gmail.com>
2025-01-12 09:53:50 +0100

    Add nav elements and configure routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { NavComponent } from './nav/nav.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [HeaderComponent, NavComponent],
+  imports: [HeaderComponent, NavComponent, RouterOutlet],
 })
 export class AppComponent {
   title = 'junhki-blog';
